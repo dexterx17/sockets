479ce34f8f4254ada25a9169f9c24b7466e181b5
Renato <renato_toasa@hotmail.com>
2015-10-14 15:41:17 -0500

    conociendo a Jquery

--- application/views/administrador/consola.php
+++ application/views/administrador/consola.php
@@ -2,8 +2,21 @@
 <html>
 <head>
 	<title>CONSOLA DE SOCKETS</title>
+	<script src="<?php echo site_url('js'); ?>/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+
+		$('#consola').append('hola taz');
+		
+	});
+	</script>
 </head>
 <body>
+
+<div id="consola">
+	
+</div>
+
 <?php 
 foreach ($paquete as $key => $value) {
 	echo $key.$value; 
